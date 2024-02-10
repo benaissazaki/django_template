b29af7340a4413160082b11c262ea6743531e4ed
lilxak <zakben45@gmail.com>
2024-02-10 14:12:01 +0100

    feat: extract env variables

--- config/settings.py
+++ config/settings.py
@@ -11,21 +11,29 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env(
+    DEBUG=(bool,),
+    SECRET_KEY=(str,),
+    ALLOWED_HOSTS=(list,)
+)
+
+env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^kr-!#*j$qmv+$s386ny^d_7@nl&w94*wnwkp!pfd@0i6*qh27'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env('ALLOWED_HOSTS')
 
 
 # Application definition
